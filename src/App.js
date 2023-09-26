58971678ad8de5bf08636f026a9758eb4a1bc44f
user <basalingappa.itagi@inqude.com>
2023-09-26 16:56:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component, useContext } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Experiance from "./components/Experiance/Experiance";
+// import TechStack from "./components/TechStack/TechStack";
+// import Projects from "./components/Projects/Projects";
+// import Contact from "./components/Contact/Contact";
+// import Footer from "./components/Footer/Footer";
+// import ScrollToTop from "react-scroll-to-top";
+// import { ThemeContext } from "./context/ThemeContext";
+
+const App = () => {
+  return (
+    <div>
+      {/* <h2>hello</h2> */}
+      <NavBar />
+    </div>
+  );
+};
+
+export default App;
